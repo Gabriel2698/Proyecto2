719478c3fd3afd62a2641f1c3b9fc13bbc648b29
Gabriel Monge Monge <josuemonge9926@gmail.com>
2024-12-16 21:21:17 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto2/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto2.Models;
+
+
+
+namespace Proyecto2.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
+        public DbSet<Admin> Adminis { get; set; }
+
+
+
+
+
+    }
+}
---
+++ Proyecto2/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿namespace Proyecto2.Models
+{
+    public class Admin
+    {
+     
+     
+            public int Id { get; set; }
+            public string Nombre { get; set; } = string.Empty;
+            public string ? Contraseña { get; set; }
+
+  
+
+
+
+
+    }
+}
